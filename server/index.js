aeff90f42c69be49d8f3c4bbd9b1f05532bcc93a
Ishita <ishitagarg1abc@gmail.com>
2021-12-21 16:31:08 +0530

    correct error

--- server/index.js
+++ server/index.js
@@ -52,10 +52,17 @@ app.post("/send", function (req, res) {
 
 const PORT = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
-    res.send("Ecommerce");
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+//     res.send("Ecommerce");
+// })
+
+app.get('/*', (req, res) => {
+    let url = path.join(__dirname, '../client/build', 'index.html');
+    if (!url.startsWith('/app/'))
+      url = url.substring(1);
+    res.sendFile(url);
+  });
 
 app.listen(PORT, () => {
     console.log("Listening on port " + PORT);
